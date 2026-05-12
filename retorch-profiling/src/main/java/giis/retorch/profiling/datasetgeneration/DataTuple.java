d404c72d06e073ae24ea20380b60723fa4c0cece
Augusto <augustocristian@uniovi.es>
2026-05-12 14:48:07 +0200

    Datatuple reviewed

--- retorch-profiling/src/main/java/giis/retorch/profiling/datasetgeneration/DataTuple.java
+++ retorch-profiling/src/main/java/giis/retorch/profiling/datasetgeneration/DataTuple.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The {@code DataTuple} class represents a data structure that holds an ID for a job, its stage,
+ * The {@code DataTuple} class represents a data structure that holds an ID for a {@code TJob}, its stage,
  * and a map with the lifecycle durations. This class it's a necessary enabler to calculate the average
  * datasets, allowing to store the duration of the different executions and its corresponding stage
  */
@@ -24,23 +24,22 @@ public class DataTuple implements Serializable {
     public String getIdTJob() {
         return idTJob;
     }
-    public void setIdTJob(String idTJob) {
-        this.idTJob = idTJob;
-    }
     public int getStage() {
         return stage;
-    }
-    public void setStage(int stage) {
-        this.stage = stage;
-    }
-
-    public void putLifeCycleDuration(String lifecycle, Double duration) {
-        getLifecycleDuration().put(lifecycle, duration);
     }
     public Map<String, Double> getLifecycleDuration() {
         return lifecycleDuration;
     }
-    public void setLifecycleDuration(Map<String, Double> lifecycleDuration) {
-        this.lifecycleDuration = lifecycleDuration;
+
+    public void setIdTJob(String idTJob) {
+        this.idTJob = idTJob;
     }
-}
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+    public void setLifecycleDuration(Map<String, Double> lifecycleDuration) {this.lifecycleDuration = lifecycleDuration;}
+
+    public void putLifeCycleDuration(String lifecycle, Double duration) {
+        getLifecycleDuration().put(lifecycle, duration);
+    }
+    }
